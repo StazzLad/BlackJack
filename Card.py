d89591e54e5ed0a152772dfa008551808c61e82a
Stazz <erk.apaydin@protonmail.com>
2023-09-21 16:38:56 +0300

    second commint

--- Card.py
+++ Card.py
@@ -1,21 +1,3 @@
-class Card:
-    def __init__(self,value,suit):
-        self.cost = value 
-        self.value = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'][value-1]
-        self.suit = '♥♦♣♠'[suit]
-
-    def price(self):
-        if self.cost >= 10:
-            return 10 
-        elif self.cost == 1:
-            return 11
-        return self.cost
-    
-    def show (self):
-        print('┌───────┐')
-        print(f'| {self.value:<2}    |')
-        print('|       |')
-        print(f'|   {self.suit}   |')
-        print('|       |')
-        print(f'|    {self.value:>2} |')
-        print('└───────┘') 
+suits = ['Hearts','Diamonds','Clubs','Spades']
+ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+card_values = {'A':11,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10}
